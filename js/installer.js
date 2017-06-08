c4b76a74a11bfb72dc57783da6267864382f7f39
Nate Mihu <natemihu@gmail.com>
2017-06-08 16:43:05 -0600

    First commit... LET THE GAMES BEGIN!!

---
+++ js/installer.js
@@ -0,0 +1,6 @@
+// Create installer that installs packages and all their dependancies.  If a package has dependancy, install dependancy prior to package.
+
+// Accept input from command line
+var prompt = require('prompt');
+// Start prompt
+prompt.start();
